3ebabbb8f2584206752b9027b28a921d4e294af7
MoonLy03 <93345815+Finn331@users.noreply.github.com>
2024-05-15 16:55:50 +0700

    penyesuaian

--- Assets/Script/Player/PlayerStatus.cs
+++ Assets/Script/Player/PlayerStatus.cs
@@ -75,7 +75,7 @@ public class PlayerStatus : MonoBehaviour
     {
         scoreText.text = "Score: " + score.ToString();
     }
-    public void ResetHealth()
+    public void Respawn()
     {
         currHealth = maxHealth;
     }
