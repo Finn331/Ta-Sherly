d23f8c71fc4864400da88ccba0ebe74b644973ff
Sanguine3116 <93345815+Finn331@users.noreply.github.com>
2024-05-27 16:11:19 +0700

    Penyesuaian

--- Assets/Script/Enemy/EnemyStatus.cs
+++ Assets/Script/Enemy/EnemyStatus.cs
@@ -23,7 +23,7 @@ public class EnemyStatus : MonoBehaviour
         // No need to call Die here.
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Projectile"))
         {
